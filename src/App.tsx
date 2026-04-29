b3bd14342777a94afbf6b7b794e1da3065bf572c
m00on13 <mansi.patel7279s@gmail.com>
2026-04-29 22:25:58 +0530

    feat: add SplashCursor component for interactive fluid-like animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
 import { HeroAboutSequence } from './components/HeroAbout/HeroAboutSequence'
+import SplashCursor from './components/SplashCursor/SplashCursor'
 
 function App() {
   return (
     <>
+      <SplashCursor
+        SIM_RESOLUTION={64}
+        DYE_RESOLUTION={512}
+        DENSITY_DISSIPATION={4.5}
+        VELOCITY_DISSIPATION={2}
+        PRESSURE={0.1}
+        CURL={3}
+        SPLAT_RADIUS={0.1}
+        SPLAT_FORCE={1500}
+        COLOR_UPDATE_SPEED={7}
+      />
       <HeroAboutSequence />
     </>
   )
